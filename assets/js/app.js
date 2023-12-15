d249ebd7ea7c5a6cfe690d55f30c86cd0915f58c
Sergey <52292140+703comua@users.noreply.github.com>
2023-12-15 21:18:39 +0200

    header done

---
+++ assets/js/app.js
@@ -0,0 +1,83 @@
+"use strict";
+
+$(document).ready(function () {
+    // Search Nav
+    $(".nav-btns__search").on("click", function () {
+        if ($(".header__search").hasClass("active")) {
+            $(".header__search").removeClass("active");
+        } else {
+            $(".header__search").addClass("active");
+        }
+
+        // hide languages
+        if ($(".header__languages").hasClass("active")) {
+            $(".header__languages").removeClass("active");
+        }
+    });
+
+    // Languages
+    $(".nav-btns__languages").on("click", function () {
+        if ($(".header__languages").hasClass("active")) {
+            $(".header__languages").removeClass("active");
+        } else {
+            $(".header__languages").addClass("active");
+        }
+
+        // search languages
+        if ($(".header__search").hasClass("active")) {
+            $(".header__search").removeClass("active");
+        }
+    });
+
+    // Aside Nav
+    $(document).click(function (event) {
+        // если мы кликнули в любом месте сайта, кроме иконки поиска (лупа)
+        if (!$(event.target).closest($(".nav-btns__search")).length && !$(event.target).closest($(".header__search")).length) {
+            $(".header__search").removeClass("active");
+        }
+        // если мы кликнули в любом месте сайта, кроме иконки текущего выбранного языка
+        if (!$(event.target).closest($(".nav-btns__languages")).length) {
+            $(".header__languages").removeClass("active");
+        }
+
+        // если мы кликнули в любом месте сайта, кроме бокового меню .nav-aside
+        if (!$(event.target).closest($(".nav-aside")).length) {
+            // console.log(event.target);
+            // если aside-меню уже открыто
+            if ($(".nav-aside").hasClass("active")) {
+                // закрываем aside-меню
+                $(".nav-aside").removeClass("active");
+                $(".header").removeClass("shadow-active");
+                $("body").removeClass("modal-open");
+            } else {
+                // если мы кликнули по кнопке .nav-btns__aside
+                // console.log($(event.target).closest('.nav-btns__aside').length);
+                if ($(event.target).closest(".nav-btns__aside").length) {
+                    $(".nav-aside").addClass("active");
+                    $(".header").addClass("shadow-active");
+                    $("body").addClass("modal-open");
+                }
+            }
+        }
+    });
+
+    $(".nav-aside__close").on("click", function () {
+        $(".nav-aside").removeClass("active");
+        $(".header").removeClass("shadow-active");
+        $("body").removeClass("modal-open");
+    });
+
+    $(".aside-catalog__chevron").on("click", function () {
+        let $thisArrow = $(this);
+        let $parentLi = $(this).parent();
+        // let $prevLink = $(this).prev('.link');
+        // let $childIcon = $(this).find('.icon');
+
+        // если список есть
+        if ($(this).next("ul").length == 1) {
+            $(this).next("ul").slideToggle();
+            // $prevLink.toggleClass('active');
+            $(this).parent("li").toggleClass("active");
+        }
+    });
+});
